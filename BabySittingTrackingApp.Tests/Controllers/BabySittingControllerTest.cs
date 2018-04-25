b08bdd9be7bd91a8546723de3cb54862fc5e2453
kspeckhals <kspeckhals@levelsetsolutions.com>
2018-04-25 17:18:04 -0400

    New Tests for the index on baby sitting controller is not null

---
+++ BabySittingTrackingApp.Tests/Controllers/BabySittingControllerTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace BabySittingTrackingApp.Tests.Controllers
+{
+    [TestClass]
+    public class BabySittingControllerTest
+    {
+        [TestMethod]
+        public void whenBabySittingControllerIndexIsLoadedVerifyItIsNotNULL()
+        {
+            // Arrange
+            BabySittingController controller = new BabySittingController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
