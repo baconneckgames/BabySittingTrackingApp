1664d17f694ca33b244919a43e6b88597586c0ef
kspeckhals <kspeckhals@levelsetsolutions.com>
2018-04-25 17:20:11 -0400

    Passing test for baby sitting controller index function not being null

---
+++ BabySittingTrackingApp/Controllers/BabySittingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BabySittingTrackingApp.Controllers
+{
+    public class BabySittingController : Controller
+    {
+        // GET: BabySitting
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
